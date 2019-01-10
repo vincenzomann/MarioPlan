d7a2bbcc1e4aeb0c03a38b9ba4202a4f2a61260c
Vincenzo Mann <vinnymann97@gmail.com>
2019-01-10 23:52:44 +0000

    connected firestore to create project action dispatch

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -5,9 +5,24 @@ export const createProject = (project) => {
   // }
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
-    dispatch({ 
-      type: 'CREATE_PROJECT', 
-      project
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project, //project title and content
+      authorFirstName: 'Vincenzo',
+      authorLastName: 'Mann',
+      authorID: 64,
+      createdAt: new Date()
+    }).then( () => {
+      dispatch({ 
+        type: 'CREATE_PROJECT', 
+        project
+      })
+    }).catch( (err) => {
+      dispatch({ 
+        type: 'CREATE_PROJECT_ERROR', 
+        err
+      })
     })
+    
   }
 };
